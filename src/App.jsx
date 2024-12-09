4556c2d3755a9950765193fee43fb80391a5f310
Alpha <d.juzu7777@gmail.com>
2024-12-09 14:57:59 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+
+const App = () => {
+    return(
+        <div>
+            <Header/>
+            <Section/>
+            <Footer/>
+        </div>
+    )
+}
+export default App;
